0416640d419477c6ad5295c134db0c1ddc2f7cf8
Icarogib <icarogib@gmail.com>
2024-10-19 20:37:12 -0300

    new: novo indexes e pacotes
    - Adicionado o pacote cors para poder conectar com o backend (express)
    - Adicionado novos index.html para testes de inputs e requisicoes com axios
    - mudancas em como as tabelas serao vistas

--- sqlscripts/createtabela.sql
+++ sqlscripts/createtabela.sql
@@ -11,7 +11,7 @@ CREATE TABLE cliente (
 
 
 CREATE TABLE vendedor (
-    vendedor_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(45) NOT NULL,
     cpf INT NOT NULL,
     email VARCHAR(40) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE vendedor (
 
 
 CREATE TABLE produto (
-    produto_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(45) NOT NULL,
     descricao VARCHAR(45) NOT NULL,
     preco DECIMAL(10, 2) NOT NULL,
@@ -30,23 +30,23 @@ CREATE TABLE produto (
 );
 
 CREATE TABLE venda (
-    venda_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     cliente_id INT NOT NULL,
     vendedor_id INT NOT NULL,
     data_venda VARCHAR(45) NOT NULL,
     total_venda DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (cliente_id) REFERENCES cliente(id),
-    FOREIGN KEY (vendedor_id) REFERENCES vendedor(vendedor_id)
+    FOREIGN KEY (vendedor_id) REFERENCES vendedor(id)
 );
 
 CREATE TABLE item_venda (
-    item_venda_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     venda_id INT NOT NULL,
     produto_id INT NOT NULL,
     quantidade INT NOT NULL,
     preco_unitario DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (venda_id) REFERENCES venda(venda_id),
-    FOREIGN KEY (produto_id) REFERENCES produto(produto_id)
+    FOREIGN KEY (venda_id) REFERENCES venda(id),
+    FOREIGN KEY (produto_id) REFERENCES produto(id)
 );
 
 CREATE TABLE forma_pagamento (
@@ -62,5 +62,5 @@ CREATE TABLE pagamento (
     data_pagamento VARCHAR(20) NOT NULL,
     status_pagamento VARCHAR(30) NOT NULL,
     FOREIGN KEY (forma_pagamento_id) REFERENCES forma_pagamento(forma_pagamento_id),
-    FOREIGN KEY (venda_id) REFERENCES venda(venda_id)
+    FOREIGN KEY (venda_id) REFERENCES venda(id)
 );
--- sqlscripts/populante.sql
+++ sqlscripts/populante.sql
@@ -1,18 +1,18 @@
-INSERT INTO clientes (nome, endereco, telefone) VALUES ('Maicon', 'Rua Jackson', 33431134);
+INSERT INTO cliente (nome, endereco, telefone) VALUES ('Maicon', 'Rua Jackson', 33431134);
 
-INSERT INTO clientes (nome, endereco, telefone) VALUES ('Julius', 'Rua Chris', 33421334);
+INSERT INTO cliente (nome, endereco, telefone) VALUES ('Julius', 'Rua Chris', 33421334);
 
-INSERT INTO clientes (nome, endereco, telefone) VALUES ('Baki', 'Av. Hanma', 99999999);
+INSERT INTO cliente (nome, endereco, telefone) VALUES ('Baki', 'Av. Hanma', 99999999);
 
-INSERT INTO clientes (nome, endereco, telefone) VALUES ('Kuririn', 'Av. Terra', 40028922);
+INSERT INTO cliente (nome, endereco, telefone) VALUES ('Kuririn', 'Av. Terra', 40028922);
 
-INSERT INTO clientes (nome, endereco, telefone) VALUES ('Gabi', 'Av. Gol', 08007778000);
+INSERT INTO cliente (nome, endereco, telefone) VALUES ('Gabi', 'Av. Gol', 08007778000);
 
-INSERT INTO clientes (nome, endereco, telefone) VALUES ('Kame', 'Rua Hame Ha', 998077335);
+INSERT INTO cliente (nome, endereco, telefone) VALUES ('Kame', 'Rua Hame Ha', 998077335);
 
-INSERT INTO clientes (nome, endereco, telefone) VALUES ('Luffy', 'Vila Fuusha', 14143200);
+INSERT INTO cliente (nome, endereco, telefone) VALUES ('Luffy', 'Vila Fuusha', 14143200);
 
-INSERT INTO clientes (nome, endereco, telefone) VALUES ('Marcelo', 'Rua Yuri', 123456789);
+INSERT INTO cliente (nome, endereco, telefone) VALUES ('Marcelo', 'Rua Yuri', 123456789);
 
 INSERT INTO estoque (nome_produto, preco, quantidade) VALUES ('Perfume Essencial Oud', 130, 10 );
 
